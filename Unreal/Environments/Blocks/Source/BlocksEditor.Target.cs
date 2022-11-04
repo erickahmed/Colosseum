ad7cee33fa98a0907b81b2896db8daa1e9b9a661
Erick <ahmederick00@gmail.com>
2022-11-04 09:47:56 +0100

    Adding compatibility for XCode v>13.3

--- Unreal/Environments/Blocks/Source/BlocksEditor.Target.cs
+++ Unreal/Environments/Blocks/Source/BlocksEditor.Target.cs
@@ -7,6 +7,10 @@ public class BlocksEditorTarget : TargetRules
 {
 	public BlocksEditorTarget(TargetInfo Target) : base(Target)
 	{
+        // If having trouble building with XCode version>13.3, uncomment following two lines:
+        // bOverrideBuildEnvironment = true;
+        // AdditionalCompilerArguments = "-Wno-unused-but-set-variable";
+
 		Type = TargetType.Editor;
 		ExtraModuleNames.AddRange(new string[] { "Blocks" });
         DefaultBuildSettings = BuildSettingsVersion.V2;
